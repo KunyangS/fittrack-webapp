54f3cfd7e4650088fe4528dec4c08863cca04612
Kunyang Xie <24447687@student.uwa.edu.au>
2025-04-22 00:05:46 +0800

    init: add database configuration and register upload module.

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 # Imports
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
 
 # Initialize Flask app, redirect the tamplate folder outside the app folder
 template_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates')
@@ -11,5 +12,16 @@ app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 # Set a secret key for session management, which needs to be refined after "Security" lecture
 app.config['SECRET_KEY'] = os.urandom(24)
 
+# Configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///fitness.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize SQLAlchemy
+db = SQLAlchemy(app)
+
 # --- Routes ---
 from app import routes
+
+# Register Blueprints
+from app.upload import upload_bp   
+app.register_blueprint(upload_bp) 
