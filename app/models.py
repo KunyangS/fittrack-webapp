a1695b70bca2a817f2c017a4bbfd2458ff4172ae
VincentWang2961 <vincentwang2961@gmail.com>
2025-05-07 01:36:21 +0800

    Refactor UserInfo model to include password management methods and user attributes

--- app/models.py
+++ app/models.py
@@ -3,13 +3,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date, time
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
-# User basic information model 
+# User basic information model
 class UserInfo(db.Model):
     __tablename__ = 'user_info'
-    
+
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     date = db.Column(db.Date, default=date.today)
     time = db.Column(db.Time)
     gender = db.Column(db.String(10), nullable=False)
@@ -18,6 +22,12 @@ class UserInfo(db.Model):
     weight = db.Column(db.Float, nullable=False)  
     fitness_entries = db.relationship('FitnessEntry', backref='user', lazy=True)
     food_entries = db.relationship('FoodEntry', backref='user', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     def __repr__(self):
         return f'<UserInfo {self.date} - {self.gender} - {self.weight}kg>'
